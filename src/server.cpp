f06aef54eb6dfbd338a1cff20e13e44addc8dd0f
nikshrma <nikshrma2006@gmail.com>
2026-06-25 22:10:16 +0530

    Refactor server loop to support graceful shutdown, add SO_REUSEADDR, and modularize request processing

--- src/server.cpp
+++ src/server.cpp
@@ -2,17 +2,30 @@
 #include "request.h"
 #include "response.h"
 #include <arpa/inet.h>
+#include <atomic>
 #include <cerrno>
+#include <csignal>
+#include <cstdio>
 #include <cstring>
 #include <iostream>
 #include <netinet/in.h>
 #include <sstream>
 #include <sys/_endian.h>
 #include <sys/_types/_ssize_t.h>
+#include <sys/signal.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
+std::atomic<bool> Server::serverRunning{true};
+void Server::signalHandler(int signal) { serverRunning = false; }
+
 void Server::listen(int port) {
+  struct sigaction sa{};
+  sa.sa_flags = 0;
+  sa.sa_handler = Server::signalHandler;
+  sigemptyset(&sa.sa_mask);
+  sigaction(SIGINT, &sa, nullptr);
+  sigaction(SIGTERM, &sa, nullptr);
   serverFd = socket(AF_INET, SOCK_STREAM, 0);
   if (serverFd < 0) {
     std::cerr << "Failed to create socket: " << std::strerror(errno) << '\n';
@@ -22,7 +35,8 @@ void Server::listen(int port) {
   address.sin_family = AF_INET;
   address.sin_addr.s_addr = INADDR_ANY;
   address.sin_port = htons(port);
-
+  int opt = 1;
+  setsockopt(serverFd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
   if (bind(serverFd, (sockaddr *)&address, sizeof(address)) < 0) {
     std::cerr << "Failed to bind: " << std::strerror(errno) << '\n';
     return;
@@ -33,36 +47,58 @@ void Server::listen(int port) {
   }
   std::cout << "listening on port -> " << port << '\n';
 
-  while (true) {
+  while (serverRunning) {
     int clientFd = accept(serverFd, nullptr, nullptr);
-    char buffer[4096];
-
-    ssize_t bytesRead = recv(clientFd, buffer, sizeof(buffer), 0);
-
-    std::string request(buffer, bytesRead);
-
-    std::istringstream stream(request);
-    std::string method;
-    std::string path;
-    std::string version;
-
-    stream >> method >> path >> version;
-    std::cout << "method:" << method << '\n'
-              << "path:" << path << '\n'
-              << "version:" << version << '\n';
-    Request req;
-    req.method = method;
-    req.path = path;
+    if (clientFd == -1) {
+      if (errno == EINTR) {
+        continue;
+      }
+      perror("accept");
+      break;
+    }
+    Request req = parseRequest(clientFd);
     Response res(clientFd);
-    if (getRoutes.find(req.path) != getRoutes.end()) {
-      getRoutes[req.path](req, res);
-    } else
-      res.send("404");
+    dispatch(req, res);
     close(clientFd);
   }
 }
 
 void Server::closeServer() { close(serverFd); }
+
 void Server::get(std::string path, Handler handler) {
   getRoutes[path] = handler;
 }
+
+Request Server::parseRequest(int clientFd) {
+  char buffer[4096];
+
+  ssize_t bytesRead = recv(clientFd, buffer, sizeof(buffer), 0);
+  if (bytesRead <= 0) {
+    return {};
+  }
+
+  std::string request(buffer, bytesRead);
+
+  std::istringstream stream(request);
+  std::string method;
+  std::string path;
+  std::string version;
+
+  stream >> method >> path >> version;
+  std::cout << "method:" << method << '\n'
+            << "path:" << path << '\n'
+            << "version:" << version << '\n';
+  Request req;
+  req.method = method;
+  req.path = path;
+  return req;
+}
+
+void Server::dispatch(Request &req, Response &res) {
+  auto route = getRoutes.find(req.path);
+  if (route != getRoutes.end()) {
+    route->second(req, res);
+  } else
+    res.send("404");
+}
+Server::~Server() { closeServer(); }
--- src/server.h
+++ src/server.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "request.h"
 #include "response.h"
+#include <atomic>
 #include <functional>
 #include <unordered_map>
 
@@ -9,9 +10,14 @@ private:
   int serverFd;
   using Handler = std::function<void(Request &, Response &)>;
   std::unordered_map<std::string, Handler> getRoutes;
+  Request parseRequest(int clientFd);
+  void dispatch(Request &, Response &);
+  void closeServer();
+  static std::atomic<bool> serverRunning;
+  static void signalHandler(int);
 
 public:
   void listen(int port);
-  void closeServer();
   void get(std::string path, Handler handler);
+  ~Server();
 };
